e1fa5e5e5a3f2ebce4fd4b90f5983b1866e783b7
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2026-01-27 09:16:14 +0100

    Project Init

---
+++ src/main/java/se/lexicon/g58todoapp/G58TodoAppApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g58todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G58TodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G58TodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g58todoapp/G58TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g58todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G58TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
